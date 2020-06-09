0bc6c370e36e99ad16786a2938139c702030ba55
Alejandra <aledev.roman@gmail.com>
2020-06-09 13:57:41 -0500

    Update dashboard.component.ts

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -10,15 +10,13 @@ export class DashboardComponent implements OnInit {
 
   user: any;
   modal = false;
-  rol: any;
-  
+
   constructor(
     public authService: AuthService,
   ) { }
 
   ngOnInit(): void {
     this.user = JSON.parse(localStorage.getItem('usuario'));
-    this.rol = this.authService.getTypeOfUser(this.user.schoolId, this.user.password);
   }
 
   openModal() {
